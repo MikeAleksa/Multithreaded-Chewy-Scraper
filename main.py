882fff23a9175f2704a1cde0c51db2919be2e238
Michael Aleksa <mtaleksa1@gmail.com>
2019-11-07 14:32:05 -0500

    minimum viable product

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ SEARCH_URL = "https://www.chewy.com/s?rh=c%3A288%2Cc%3A332&page="  # contains al
 def main():
     logger = VerboseScraperLogger()
     scraper = Scraper(database=DATABASE, logger=logger, num_threads=THREADS)
-    # scraper.scrape(url=SEARCH_URL, pages_of_results=97)
 
-    url = "https://www.chewy.com/tiki-dog-taste-france-beef-potatoes/dp/212988"
-    scraper.scrape_food_if_new(url)
+    # scrape all dog foods
+    scraper.scrape(url=SEARCH_URL, pages_of_results=97)
+
+    # scrape any individual foods here
+    # single_url = "https://www.chewy.com/billy-margot-single-animal-protein/dp/206119"
+    # scraper.scrape_food_if_new(single_url)
 
 
 if __name__ == "__main__":
