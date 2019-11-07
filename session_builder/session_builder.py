4a7bbe32d8aaf5b3a405aa036a547d42272b2a24
mike <mtaleksa1@gmail.com>
2019-11-07 18:54:37 -0500

    refactor SessionBuilder - add docstrings

--- session_builder/session_builder.py
+++ session_builder/session_builder.py
@@ -13,39 +13,55 @@ class SessionBuilder:
     Proxies are obtained using an API key from ProxyBonanza
 
     If no useragents are supplied, a default Firefox useragent is used
+
     If no API data is supplied for ProxyBonanza, no proxies are used
     """
 
     def __init__(self, api_data: dict = None, useragents: list = None):
+        """
+        set up cycle of useragents and list of proxies
+        :param api_data: dict containing api_key and api_url from proxybonanza
+        :param useragents: list of browser useragents
+        """
         # set up useragents
         if useragents is not None:
             self.useragents = cycle(useragents)
         else:
             self.useragents = cycle(["Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"])
 
-        # set up proxies using api-url and api-key for proxybonanza, if supplied
-        self.proxies = None
-        if api_data is not None:
-            response = requests.get(api_data["api-url"], headers={"Authorization": api_data["api-key"]})
+        # set up proxies using api-url and api-key for proxybonanza if supplied
+        self.api_data = api_data
+        self.proxies = self.get_proxies_from_proxybonanza()
+
+    def get_proxies_from_proxybonanza(self) -> list:
+        """
+        Get a list of proxies from proxybonanza using an api-key and an api-url, if supplied
+        :return: list of proxies from proxybonanza, or None if no api data supplied to SessionHelper object
+        """
+        proxies = None
+
+        if self.api_data is not None:
+            response = requests.get(self.api_data["api-url"], headers={"Authorization": self.api_data["api-key"]})
             if not response.ok:
                 response.raise_for_status()
             else:
                 proxy_data = json.loads(response.content)["data"]
                 login: str = proxy_data["login"]
                 password: str = proxy_data["password"]
-                self.proxies = []
+                proxies = []
                 for proxy in proxy_data["ippacks"]:
                     ip: str = proxy["ip"]
                     port: str = str(proxy["port_http"])
                     full_proxy_string = "http://{}:{}@{}:{}".format(login, password, ip, port)
                     self.proxies.append({"http": full_proxy_string, "https": full_proxy_string})
 
+        return proxies
+
     def create_session(self) -> requests.session:
         """
         Create and return a new session using a new useragent and a random proxy from the list
-        Use own ip if no proxies specified
-        Use default Firefox/15.0.1 useragent if no useragents were specified
-        :return: requests.session object
+        Use own ip if no proxies specified, or default to Firefox/15.0.1 useragent if no useragents were specified
+        :return: requests.session object using the useragent and a random proxy
         """
         session = requests.Session()
         session.headers = {"User-Agent": next(self.useragents)}
